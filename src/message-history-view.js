d11dcb743b0fbb26114cdb69471e493ac7719dd7
John Whittenburg <jawhittenburg@gmail.com>
2016-03-31 11:47:02 -0500

    fix to history

--- src/message-history-view.js
+++ src/message-history-view.js
@@ -1,43 +1,19 @@
 'use strict';
-import token from 'slack-token';
-// 1)
-// write a fetch
-// gets the history for a hard coded channel id (C0W8YH2QP)
-// console.log the messages returned from ^
+import slackToken from 'slack-token';
+import * as MS from 'message-sender';
+
+console.log(MS);
+
 export default class MessageHistoryView {
-  constructor(element) {
-    this.element = element;
-
-    this.historyFetch();
-  }
-
-  historyFetch() {
+  getHistory (channelId) {
+    // const channelId = `C0WBYH2QP`;
     const url = `
-    https://slack.com/api/channels.history?
-    token=${token}`;
+      https://slack.com/api/channels.history?
+      token=${slackToken}&
+      channel=${channelId}`;
 
-    return fetch(url, {method: `POST`})
-    .then(response => response.json())
-    .then((info) => {
-      console.log(info.messages.text);
-    });
+    fetch(url)
+      .then(response => response.json())
+      .then(data => console.log(data.messages[0]))
   }
 }
-
-// stuff
-
-// 2)
-// function takes a channel id, fetches that channel
-// function goGetChannelStuff(id) {
-//   // stuff
-// }
-// goGetChannelStuff("C0W8YH2QP")
-
-// 3)
-// function takes channel name
-// goGetChannelStuffByName("slack_practice")
-// looks up the channel id
-// _uses_ that in function from #2
-
-// Still be done:
-// put those messages on the page instead of console.logging them
